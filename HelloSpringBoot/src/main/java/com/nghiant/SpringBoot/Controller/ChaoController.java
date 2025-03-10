8d7646157f3bc1c2f786e4a328814c0c086eafde
NguyenThanhNghia <147211357+Nghiakuu@users.noreply.github.com>
2025-03-10 15:00:10 +0700

    HelloSpringBoot

---
+++ HelloSpringBoot/src/main/java/com/nghiant/SpringBoot/Controller/ChaoController.java
@@ -0,0 +1,14 @@
+package com.nghiant.SpringBoot.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class ChaoController {
+
+		@GetMapping("/chao")
+		public String chao( ModelMap m ) {
+			m.addAttribute("msg", "Chào các ban");
+			return "Hello";
+		}	
+}
